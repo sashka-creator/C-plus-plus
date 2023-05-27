fb5cec82f8666e27225296b721843280f7bd5941
Sasha Onyshchenko <80901541+sashka-creator@users.noreply.github.com>
2023-05-27 23:55:26 +0300

    Add files via upload

---
+++ step 21.05.2023/step/main.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+
+int main() {
+    std::string filename = "input.txt";
+    std::map<std::string, int> wordFrequency;
+
+    std::ifstream inputFile(filename);
+    if (!inputFile.is_open()) {
+        std::cerr << "Couldnt Open File: " << filename << std::endl;
+        return 1;
+    }
+
+    std::string word;
+    while (inputFile >> word) {
+        // ��������� ���������� ��� �� �����
+        std::cout << "Word: " << word << std::endl;
+
+        // ��������� ������� ����� ����� � ��������
+        wordFrequency[word]++;
+    }
+
+    inputFile.close();
+
+    std::string mostFrequentWord;
+    int maxFrequency = 0;
+    for (const auto& pair : wordFrequency) {
+        std::cout << "Word '" << pair.first << "' have been wroten " << pair.second << " times." << std::endl;
+        if (pair.second > maxFrequency) {
+            maxFrequency = pair.second;
+            mostFrequentWord = pair.first;
+        }
+    }
+
+    std::cout << "The most used word: '" << mostFrequentWord << "', have been wroten " << maxFrequency << " times." << std::endl;
+
+    std::string outputFilename = "output.txt";
+    std::ofstream outputFile(outputFilename);
+    if (!outputFile.is_open()) {
+        std::cerr << "Couldn't open file for writing: " << outputFilename << std::endl;
+        return 1;
+    }
+
+    for (const auto& pair : wordFrequency) {
+        outputFile << "Word '" << pair.first << "' have been wroten " << pair.second << " times." << std::endl;
+    }
+
+    outputFile.close();
+
+    std::cout << "Result was wroten if file: " << outputFilename << std::endl;
+
+    return 0;
+}
