3a7724e9b44b5996b5ba49fe7a7c42ea393fb352
Sasha Onyshchenko <80901541+sashka-creator@users.noreply.github.com>
2023-05-31 19:03:17 +0300

    +

---
+++ step 27.05.2023/step/main.cpp
@@ -0,0 +1,42 @@
+﻿#include <vector>
+#include <algorithm>
+#include <iostream>
+
+class LambdaSorter {
+public:
+    std::vector<int> sort(const std::vector<int>& numbers, bool reverse = false) const {
+        std::vector<int> sorted_numbers = numbers;
+        std::sort(sorted_numbers.begin(), sorted_numbers.end(),
+            [](int a, int b) { return a < b; });
+
+        if (reverse) {
+            std::reverse(sorted_numbers.begin(), sorted_numbers.end());
+        }
+
+        return sorted_numbers;
+    }
+};
+
+int main() {
+    std::vector<int> numbers = { 5, 2, 8, 1, 9 };
+
+    LambdaSorter sorter;
+    std::vector<int> ascending_sorted_numbers = sorter.sort(numbers);
+    std::vector<int> descending_sorted_numbers = sorter.sort(numbers, true);
+
+    std::cout << "Ascending order: ";
+    for (int num : ascending_sorted_numbers) {
+        std::cout << num << " ";
+    }
+
+    std::cout << std::endl;
+
+    std::cout << "Descending order: ";
+    for (int num : descending_sorted_numbers) {
+        std::cout << num << " ";
+    }
+
+    return 0;
+}
+
+
