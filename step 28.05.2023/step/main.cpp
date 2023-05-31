25c62c6a46a03047a2cdb9733fe956826fc5d6da
Sasha Onyshchenko <80901541+sashka-creator@users.noreply.github.com>
2023-05-31 19:06:17 +0300

    Add files via upload

---
+++ step 28.05.2023/step/main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <thread>
+
+void threadFunction(int threadId) {
+    std::cout << "Hello from thread " << threadId << "!" << std::endl;
+}
+
+int main() {
+
+    const int numThreads = 5;
+    std::thread threads[numThreads];
+
+    for (int i = 0; i < numThreads; ++i) {
+        threads[i] = std::thread(threadFunction, i);
+    }
+
+    for (int i = 0; i < numThreads; ++i) {
+        threads[i].join();
+    }
+
+    return 0;
+}
