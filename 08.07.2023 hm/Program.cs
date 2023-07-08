6c315e5ebe24cf03870610152a365f6552da3dcd
Sasha Onyshchenko <80901541+sashka-creator@users.noreply.github.com>
2023-07-08 21:53:04 +0300

    Add files via upload

---
+++ 08.07.2023 hm/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+class Program1
+{
+    static void Main()
+    {
+        Console.Write("Введіть число від 1 до 100: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number >= 1 && number <= 100)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Помилка: число не знаходиться в діапазоні від 1 до 100.");
+        }
+    }
+}
+
+class Program2
+{
+    static void Main()
+    {
+        Console.Write("Введіть число: ");
+        double value = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть відсоток: ");
+        double percentage = Convert.ToDouble(Console.ReadLine());
+
+        double result = (value * percentage) / 100;
+        Console.WriteLine("Результат: " + result);
+    }
+}
+
+class Program3
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть чотири цифри:");
+
+        Console.Write("Перша цифра: ");
+        int digit1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Друга цифра: ");
+        int digit2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Третя цифра: ");
+        int digit3 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Четверта цифра: ");
+        int digit4 = Convert.ToInt32(Console.ReadLine());
+
+        int number = digit1 * 1000 + digit2 * 100 + digit3 * 10 + digit4;
+        Console.WriteLine("Створене число: " + number);
+    }
+}
+
---
+++ 08.07.2023 practice/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+class Program1
+{
+    static void Main()
+    {
+        Console.WriteLine("It's easy to win forgiveness for being wrong;");
+        Console.WriteLine("being right is what gets you into real trouble.");
+        Console.WriteLine("Bjarne Stroustrup");
+    }
+}
+
+class Program2
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть п'ять чисел:");
+
+        double sum = 0;
+        double max = double.MinValue;
+        double min = double.MaxValue;
+        double product = 1;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Число " + (i + 1) + ": ");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            sum += number;
+            if (number > max)
+                max = number;
+            if (number < min)
+                min = number;
+            product *= number;
+        }
+
+        Console.WriteLine("Сума: " + sum);
+        Console.WriteLine("Максимум: " + max);
+        Console.WriteLine("Мінімум: " + min);
+        Console.WriteLine("Добуток: " + product);
+    }
+}
+
+class Program3
+{
+    static void Main()
+    {
+        Console.Write("Введіть шестизначне число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int reversedNumber = 0;
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            reversedNumber = (reversedNumber * 10) + digit;
+            number /= 10;
+        }
+
+        Console.WriteLine("Результат: " + reversedNumber);
+    }
+}
+
+class Program4
+{
+    static void Main()
+    {
+        Console.Write("Введіть початок діапазону: ");
+        int start = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введіть кінець діапазону: ");
+        int end = Convert.ToInt32(Console.ReadLine());
+
+        int first = 0;
+        int second = 1;
+
+        Console.Write("Числа Фібоначчі в заданому діапазоні: ");
+
+        while (first <= end)
+        {
+            if (first >= start)
+                Console.Write(first + " ");
+
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+
+        Console.WriteLine();
+    }
+}
+
