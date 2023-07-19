2442888a282e14144409787f1667399ce089c152
Sasha Onyshchenko <80901541+sashka-creator@users.noreply.github.com>
2023-07-19 18:16:45 +0300

    Add files via upload

---
+++ 08.07.2023 hm/CS-1/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+public interface ICalc
+{
+    int Less(int valueToCompare);
+    int Greater(int valueToCompare);
+}
+
+public class Array : ICalc
+{
+    private int[] data;
+
+    public Array(int[] arr)
+    {
+        data = arr;
+    }
+
+    public int Less(int valueToCompare)
+    {
+        int count = 0;
+        foreach (var item in data)
+        {
+            if (item < valueToCompare)
+                count++;
+        }
+        return count;
+    }
+
+    public int Greater(int valueToCompare)
+    {
+        int count = 0;
+        foreach (var item in data)
+        {
+            if (item > valueToCompare)
+                count++;
+        }
+        return count;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] dataArray = { 10, 15, 20, 25, 30 };
+        Array array = new Array(dataArray);
+
+        int valueToCompare = 22;
+
+        int lessCount = array.Less(valueToCompare);
+        int greaterCount = array.Greater(valueToCompare);
+
+        Console.WriteLine($"Number of elements less than {valueToCompare}: {lessCount}");
+        Console.WriteLine($"Number of elements greater than {valueToCompare}: {greaterCount}");
+    }
+}
+
+///
+
+public interface IOutput2
+{
+    void ShowEven();
+    void ShowOdd();
+}
+
+public class Array : IOutput2
+{
+    private int[] data;
+
+    public Array(int[] arr)
+    {
+        data = arr;
+    }
+
+    public void ShowEven()
+    {
+        Console.WriteLine("Even elements:");
+        foreach (var item in data)
+        {
+            if (item % 2 == 0)
+                Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public void ShowOdd()
+    {
+        Console.WriteLine("Odd elements:");
+        foreach (var item in data)
+        {
+            if (item % 2 != 0)
+                Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] dataArray = { 10, 15, 20, 25, 30 };
+        Array array = new Array(dataArray);
+
+        array.ShowEven();
+        array.ShowOdd();
+    }
+}
